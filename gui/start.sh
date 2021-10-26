86585e2dba333fb4259d0d9a6f0c44f53422694b
Rahul Thakoor <rahul.thakoor@gmail.com>
2021-10-26 06:33:19 +0400

    wip

--- gui/start.sh
+++ gui/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 
 # Wait for the x11 socket to appear in the shared volume
-while [ ! -e /tmp/.X11-unix/X${DISPLAY#*:} ]; do sleep 0.1; done
+# while [ ! -e /tmp/.X11-unix/X${DISPLAY#*:} ]; do sleep 0.1; done
 
 python hello.py
+
+balena-idle
